055588a3d50668d5254dd79f6446c40237dd6d0e
saraa92 <saralshaikhmubarak@hotmail.com>
2017-10-10 18:44:57 +0300

    Add files via upload

--- vendor/bootstrap/js/calc.js
+++ vendor/bootstrap/js/calc.js
@@ -6,16 +6,14 @@ var estimateSavings = function () {
   var currentUsage = document.getElementById("currentUsage").value;
   document.getElementById("solarSystem").innerHTML = solarSystem;
 
-  if (1 < currentUsage <= 4000) {
-  solarSystem=("1.5 kW");
+  if (1 < currentUsage && currentUsage <= 4000) {
+  solarSystem="1.5 kW";
   }
-  
-  else if (4001 < currentUsage <= 6000) {
-  solarSystem=("3 kW");
+  else if (4001 < currentUsage && currentUsage <= 6000) {
+  solarSystem="3 kW";
   }
-
   else if (currentUsage > 6000) {
-  solarSystem=("5 kW");
+  solarSystem="5 kW";
   }
 
   document.getElementById("solarSystem").innerHTML = solarSystem;
